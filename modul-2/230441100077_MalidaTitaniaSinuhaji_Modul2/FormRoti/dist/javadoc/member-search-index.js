ad711e5d82caa120a1b2a88649d9832684d038ae
Ediputra07 <edi075412@gmail.com>
2024-10-09 20:55:51 +0700

    230441100077_MalidaTitaniaSinuhaji_Modul2

---
+++ modul-2/230441100077_MalidaTitaniaSinuhaji_Modul2/FormRoti/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Frame2","l":"Frame2()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Frame2","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();

e0ecf35a0984dc30b9c90b5397689fd54951dcdb
firlianaeka12 <ekahafidz644@gmail.com>
2024-11-07 19:09:26 +0700

    MODUL3_230441100169_FirlianaEkaKurniaSari_AsprakKakRaka

---
+++ modul-3/230441100169_FIRLIANA EKA KURNIA SARI_MODUL3/src/pkg230441100169_firliana/eka/kurnia/sari_modul3/tfbayar.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg230441100169_firliana.eka.kurnia.sari_modul3;
+
+import java.awt.event.KeyAdapter;
+
+/**
+ *
+ * @author ASUS
+ */
+class tfbayar {
+
+    static void addKeyListener(KeyAdapter keyAdapter) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
